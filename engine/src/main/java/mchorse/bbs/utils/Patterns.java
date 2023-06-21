2e757c9dca005e32f7a722891a2ddb7fb66866d5
McHorse <mchorselessrider@gmail.com>
2023-06-21 21:30:14 +0100

    Allow any letters in filename fields (Centryfuga)

--- engine/src/main/java/mchorse/bbs/utils/Patterns.java
+++ engine/src/main/java/mchorse/bbs/utils/Patterns.java
@@ -4,6 +4,6 @@ import java.util.regex.Pattern;
 
 public class Patterns
 {
-    public static final Pattern FILENAME = Pattern.compile("^[\\w\\d-_.\\[\\]!@#$%^&()]*$");
+    public static final Pattern FILENAME = Pattern.compile("^[\\p{L}\\d-_.\\[\\]!@#$%^&()]*$");
     public static final Pattern UNICODE_CHARACTER = Pattern.compile("\\\\u([\\da-zA-Z]{4})");
 }
