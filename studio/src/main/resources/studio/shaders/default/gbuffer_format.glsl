6f83ca328f258a454705a9a376d119801f1d78f9
McHorse <mchorselessrider@gmail.com>
2023-08-20 08:53:44 +0100

    Remove duplicate files

--- studio/src/main/resources/studio/shaders/default/gbuffer_format.glsl
+++
@@ -1,4 +0,0 @@
-layout(location = 0) out vec4 out_color;
-layout(location = 1) out vec4 out_vertex;
-layout(location = 2) out vec4 out_normal;
-layout(location = 3) out vec4 out_lighting;
--- studio/src/main/resources/studio/shaders/default/lightmap.glsl
+++
@@ -1,6 +0,0 @@
-uniform sampler2D u_lightmap;
-
-void apply_lightmap(inout vec4 result, vec2 coords)
-{
-    result.rgb *= texture(u_lightmap, coords).rgb;
-}
